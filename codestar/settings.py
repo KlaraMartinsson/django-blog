87ca545742e2c3d659718f0fc8b861d435bed248
KlaraMartinsson <>
2023-12-19 16:03:44 +0000

    import os

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure--@d!rdf^z+rns)8$%jybu%^k9-k&hex6_ld8xbqv&i7))azy&^'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
